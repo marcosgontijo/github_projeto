b068d20f8035272d6402fc245f2bcd4fcc95f999
marcosgontijo <marcoseduardogontijo@gmail.com>
2023-11-02 00:26:06 -0300

    Criando requisicao api github e retornando usuario desejado com tratativa no metodo

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { UserProps } from "../types/user";
+
+import Search from "../components/Search";
+
+import { useState } from "react"
 
 const Home = () => {
-    return <div>Home</div>;
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async(userName: string) => {
+
+        //Requisicao api para consultar users
+        const res = await fetch(`https://api.github.com/users/${userName}`);
+
+        const data = await res.json();
+
+        console.log(data);
+
+    }
+
+    return (
+        <div>
+            <Search loadUser = {loadUser}/>
+        </div>
+    ); 
 };
 
-export default Home
+export default Home;
